11300e51b32d9b49f6774e59bddaf44755b67a78
Lu Zhao <lzhao819@usc.edu>
2021-01-09 23:14:56 -0800

    add test

--- src/test/java/com/usc/o2o/service/ShopServiceTest.java
+++ src/test/java/com/usc/o2o/service/ShopServiceTest.java
@@ -1,5 +1,7 @@
 package com.usc.o2o.service;
 
+import com.usc.o2o.BaseTest;
+import com.usc.o2o.dto.ShopExecution;
 import com.usc.o2o.entity.Area;
 import com.usc.o2o.entity.PersonInfo;
 import com.usc.o2o.entity.Shop;
@@ -10,41 +12,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 
-//public class ShopServiceTest {
-//    @Autowired
-//    private ShopService shopService;
-//
-//    @Test
-//    public void testAddShop(){
-//        Shop shop = new Shop();
-//        PersonInfo owner = new PersonInfo();
-//        Area area = new Area();
-//        ShopCategory shopCategory = new ShopCategory();
-//        owner.setUserId(1L);
-//        area.setAreaId(2L);
-//        shopCategory.setShopCategoryId(1L);
-//        shop.setOwnerId(1L);
-//        shop.setArea(area);
-//        shop.setShopCategory(shopCategory);
-//        shop.setShopName("测试的店铺1");
-//        shop.setShopDesc("test1");
-//        shop.setShopAddr("test1");
-//        shop.setPhone("test1");
-//        shop.setCreateTime(new Date());
-//        shop.setEnableStatus(ShopStateEnum.CHECK.getState());
-//        shop.setAdvice("审核中");
-//        DiskFileItemFactory factory = new DiskFileItemFactory();
-//        FileItem fi = factory.createItem("formFieldName", "application/zip", false,
-//                "/Users/lu/Documents/project/CampusMall/src/assets/picUpdate1.jpeg");
-//
-//        CommonsMultipartFile shopImg = new CommonsMultipartFile(fi);
-//        shopService.addShop(shop, shopImg);
-//        int effectedNum = shopDao.insertShop(shop);
-//        assertEquals(1, effectedNum);
-//
-//    }
-//}
+public class ShopServiceTest extends BaseTest {
+    @Autowired
+    private ShopService shopService;
+
+    @Test
+    public void testAddShop() throws FileNotFoundException {
+        Shop shop = new Shop();
+        PersonInfo owner = new PersonInfo();
+        Area area = new Area();
+        ShopCategory shopCategory = new ShopCategory();
+        owner.setUserId(1L);
+        area.setAreaId(2L);
+        shopCategory.setShopCategoryId(1L);
+        shop.setOwnerId(1L);
+        shop.setArea(area);
+        shop.setShopCategory(shopCategory);
+        shop.setShopName("测试的店铺3");
+        shop.setShopDesc("test3");
+        shop.setShopAddr("test3");
+        shop.setPhone("test3");
+        shop.setCreateTime(new Date());
+        shop.setEnableStatus(ShopStateEnum.CHECK.getState());
+        shop.setAdvice("审核中");
+
+        File shopImg = new File( "/Users/lu/Documents/project/CampusMall/src/assets/pic.jpeg");
+        InputStream is = new FileInputStream(shopImg);
+        ShopExecution se = shopService.addShop(shop, is, shopImg.getName());
+        assertEquals(ShopStateEnum.CHECK.getState(), se.getState());
+    }
+}
