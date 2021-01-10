08f9d8a80a4047cc5dd3a01f4692435c45acb830
Lu Zhao <lzhao819@usc.edu>
2021-01-09 23:12:16 -0800

    add

--- src/main/java/com/usc/o2o/service/ShopService.java
+++ src/main/java/com/usc/o2o/service/ShopService.java
@@ -2,9 +2,12 @@ package com.usc.o2o.service;
 
 import com.usc.o2o.dto.ShopExecution;
 import com.usc.o2o.entity.Shop;
+import com.usc.o2o.exceptions.ShopOperationException;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
-public interface ShopService {
-    ShopExecution addShop(Shop shop, CommonsMultipartFile shopImg) throws RuntimeException;
+import java.io.File;
+import java.io.InputStream;
 
+public interface ShopService {
+    ShopExecution addShop(Shop shop, InputStream shopInputStreamInputStream, String fileName) throws ShopOperationException;
 }
