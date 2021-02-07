8da188c5207c26e6967ff27bb5bc7967dec2ec2c
Lu Zhao <lzhao819@usc.edu>
2021-02-07 17:50:01 +0800

    9-3

--- src/main/java/com/usc/o2o/web/frontend/FrontendController.java
+++ src/main/java/com/usc/o2o/web/frontend/FrontendController.java
@@ -1,59 +1,59 @@
 package com.usc.o2o.web.frontend;
 
-import com.usc.o2o.entity.HeadLine;
-import com.usc.o2o.entity.ShopCategory;
-import com.usc.o2o.service.HeadLineService;
-import com.usc.o2o.service.ShopCategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/frontend")
 public class FrontendController {
-    @Autowired
-    private ShopCategoryService shopCategoryService;
-    @Autowired
-    private HeadLineService headLineService;
+    @RequestMapping(value = "/mainpage", method = RequestMethod.GET)
+    private String showMainPage() {
+        return "frontend/mainpage";
+    }
 
-    /**
-     * 初始化前端展示系统主页信息，包括一级店铺类别列表以及头条列表
-     * @return
-     */
-    @RequestMapping(value = "/listmainpageinfo", method = RequestMethod.GET)
-    @ResponseBody
-    private Map<String, Object> list1stShopCategory() {
-        Map<String, Object> modelMap = new HashMap<String, Object>();
-        List<ShopCategory> shopCategoryList = new ArrayList<ShopCategory>();
-        try {
-            shopCategoryList = shopCategoryService
-                    .getShopCategoryList(null);
-            modelMap.put("shopCategoryList", shopCategoryList);
-        } catch (Exception e) {
-            modelMap.put("success", false);
-            modelMap.put("errMsg", e.getMessage());
-            return modelMap;
-        }
-        List<HeadLine> headLineList = new ArrayList<HeadLine>();
-        try {
-            //获取状态为可用（1）的头条列表
-            HeadLine headLineCondition = new HeadLine();
-            headLineCondition.setEnableStatus(1);
-            headLineList = headLineService.getHeadLineList(headLineCondition);
-            modelMap.put("headLineList", headLineList);
-        } catch (Exception e) {
-            modelMap.put("success", false);
-            modelMap.put("errMsg", e.getMessage());
-            return modelMap;
-        }
-        modelMap.put("success", true);
-        return modelMap;
+    @RequestMapping(value = "/productdetail", method = RequestMethod.GET)
+    private String showProductDetail() {
+        return "frontend/productdetail";
+    }
+
+    @RequestMapping(value = "/shopdetail", method = RequestMethod.GET)
+    private String showShopDetail() {
+        return "frontend/shopdetail";
+    }
+
+    @RequestMapping(value = "/shoplist", method = RequestMethod.GET)
+    private String showShopList() {
+        return "frontend/shoplist";
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    private String index() {
+        return "frontend/index";
+    }
+
+    @RequestMapping(value = "/mypoint", method = RequestMethod.GET)
+    private String myPoint() {
+        return "frontend/mypoint";
+    }
+
+    @RequestMapping(value = "/myrecord", method = RequestMethod.GET)
+    private String myRecord() {
+        return "frontend/myrecord";
+    }
+
+    @RequestMapping(value = "/pointrecord", method = RequestMethod.GET)
+    private String pointRecord() {
+        return "frontend/pointrecord";
+    }
+
+    @RequestMapping(value = "/awarddetail", method = RequestMethod.GET)
+    private String awardDetail() {
+        return "frontend/awarddetail";
+    }
+
+    @RequestMapping(value = "/customerbind", method = RequestMethod.GET)
+    private String customerBind() {
+        return "frontend/customerbind";
     }
 }
